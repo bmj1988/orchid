c84de7e2c8a679fe7ad0bb479a8abf893ee56ff3
Brian Johnson <126127671+bmj1988@users.noreply.github.com>
2024-07-22 20:02:52 -0400

    Added support page component but haven't wired it up yet

--- tulip/app/support/index.jsx
+++ tulip/app/support/index.jsx
@@ -1,5 +1,5 @@
-import { Button, SafeAreaView, StyleSheet, TextInput } from "react-native"
-import { tulipColors } from '@/constants/Colors'
+import { Button, SafeAreaView, StatusBar, StyleSheet, TextInput } from "react-native"
+import { tulipColors } from "@/constants/Colors";
 
 
 
@@ -14,13 +14,13 @@ const SupportPage = () => {
     }
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <View>
                 <Image style={styles.img} source={{ uri: 'https://i.gyazo.com/7be89f6a141b3e4f37b2c9608ca0fcb9.png' }} />
-                <Text>
+                <Text style={styles.text}>
                     {"Tulip values the experience of its users. If you need support with any aspect of our platform, feel free to leave us a message below."}
                 </Text>
-                {!submitted && <View>
+                {!submitted && <View style={styles.view}>
                     <TextInput
                         numberOfLines={4}
                         onChangeText={text => setMessage(text)}
@@ -28,10 +28,11 @@ const SupportPage = () => {
                     />
                     <Button
                         title={"Submit"}
+                        color={tulipColors.tulipBlue}
                         onPress={() => submit()}
                     />
                 </View>}
-                {submitted && <Text>{'Message submitted'}</Text>}
+                {submitted && <Text style={styles.text}>{'Message submitted'}</Text>}
             </View>
         </SafeAreaView>
     )
@@ -39,16 +40,22 @@ const SupportPage = () => {
 
 const styles = StyleSheet.create({
     container: {
+        display: "flex",
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
 
     },
     view: {
-
+        justifyContent: "center",
+        alignItems: "center"
     },
     text: {
         color: tulipColors.tulipBlack
     },
     button: {
-
+        width: 200,
+        height: 30,
+        borderRadius: 8
     },
     img: {
         height: 100
